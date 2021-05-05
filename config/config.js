633cf487c04f72c807c3fdcbe7dfd8bf3bcb715b
rob <robert_regan86@yahoo.com>
2021-05-05 16:17:32 -0400

    jsj

---
+++ config/config.js
@@ -0,0 +1,47 @@
+exports.creds = {
+    identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', 
+
+    clientID: 'c69dc3d5-aea2-412a-b031-b06f85e5006f',
+  
+    clientSecret: 'M~x~jYDJsq2w~w6HB38j_qV1fAPIzh1nkv', 
+  
+    responseType: 'code id_token', 
+  
+    responseMode: 'form_post', 
+  
+    redirectUrl: 'https://todo-ms-auth.herokuapp.com/auth/openid/return', 
+  
+    allowHttpForRedirectUrl: true,
+  
+    validateIssuer: false,
+  
+    issuer: null,
+  
+    passReqToCallback: false,
+  
+    useCookieInsteadOfSession: false,
+  
+    cookieEncryptionKeys: [ 
+      { 'key': '12345678901234567890123456789012', 'iv': '123456789012' },
+      { 'key': 'abcdefghijklmnopqrstuvwxyzabcdef', 'iv': 'abcdefghijkl' }
+    ],
+  
+    scope: ['profile', 'offline_access', 'https://graph.microsoft.com/mail.read'],
+  
+    loggingLevel: false,
+  
+    nonceLifetime: null,
+  
+    nonceMaxAmount: 5,
+  
+    clockSkew: null,
+  };
+  
+  exports.destroySessionUrl = 'http://localhost:2121';
+  
+  exports.useMongoDBSessionStore = false;
+  
+  exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
+  
+  exports.mongoDBSessionMaxAge = 24 * 60 * 60;  
+  
